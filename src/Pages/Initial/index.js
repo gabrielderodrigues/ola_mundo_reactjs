682957d730e77c1942ba46068d26d3ea63d22754
gabrielrodrigues <g.de.lima.rodrigues@avanade.com>
2023-11-06 11:10:37 -0300

    Trabalhando com rotas aninhadas

--- src/Pages/Initial/index.js
+++ src/Pages/Initial/index.js
@@ -1,20 +1,15 @@
-import Banner from "components/Banner";
 import styles from "./Initial.module.css";
 import posts from 'json/posts.json'
 import Post from "components/Post";
 
 export default function Initial() {
   return (
-    <main>
-      <Banner />
-
-      <ul className={styles.posts}>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <Post post={post} />
-          </li>
-        ))}
-      </ul>
-    </main>
+    <ul className={styles.posts}>
+      {posts.map((post) => (
+        <li key={post.id}>
+          <Post post={post} />
+        </li>
+      ))}
+    </ul>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Initial from "./pages/Initial";
 import AboutMe from "./pages/AboutMe";
 import Menu from "./components/Menu";
 import Footer from "components/Footer";
+import DefaultPage from "components/DefaultPage";
 
 function AppRoutes() {
   return (
@@ -10,15 +11,17 @@ function AppRoutes() {
       <Menu />
       
       <Routes>
-        <Route 
-          path="/" 
-          element={<Initial />}
-        />
+        <Route path="/" element={<DefaultPage />}>
+          <Route 
+            path="/" 
+            element={<Initial />}
+          />
 
-        <Route 
-          path="/about-me" 
-          element={<AboutMe />}
-        />
+          <Route 
+            path="/about-me" 
+            element={<AboutMe />}
+          />
+        </Route>
 
         <Route 
           path="*" element={<h1>Page Not Found</h1>}
