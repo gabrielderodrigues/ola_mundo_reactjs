17509e706749f7e3dc2c8006586efd958e67c543
gabrielrodrigues <g.de.lima.rodrigues@avanade.com>
2023-11-07 15:16:59 -0300

    Instalando biblioteca MarkDown

--- src/Pages/Post/index.js
+++ src/Pages/Post/index.js
@@ -1,9 +1,26 @@
 import { useParams } from "react-router-dom";
+import ReactMarkdown from "react-markdown"
+import posts from "json/posts.json";
+import ModelPost from "components/ModelPost";
+import "./Post.css";
 
 export default function Post() {
   const parameters = useParams();
 
+  const post = posts.find((post) => {
+    return post.id === Number(parameters.id);
+  });
+
   return (
-    <h1>Teste</h1>
+    <ModelPost
+      fotoCapa={`/assets/posts/${post.id}/capa.png`}
+      titulo={post.titulo}
+    >
+      <div className="post-markdown-container">
+        <ReactMarkdown>
+          {post.texto}
+        </ReactMarkdown>
+      </div>
+    </ModelPost>
   );
 }
